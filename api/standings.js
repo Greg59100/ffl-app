a4e6aa80c2c2ecdb6f333eb3a169c3ff6bbabc66
Greg59100 <gregoire1914@gmail.com>
2026-03-14 21:52:14 +0100

    Update standings.js

--- api/standings.js
+++ api/standings.js
@@ -1,11 +1,14 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   
-  const response = await fetch(
-    'https://api.football-data.org/v4/competitions/FL1/standings',
-    { headers: { 'X-Auth-Token': process.env.FOOTBALL_API_KEY } }
-  );
-  
-  const data = await response.json();
-  res.status(200).json(data);
+  try {
+    const response = await fetch(
+      'https://api.football-data.org/v4/competitions/FL1/standings',
+      { headers: { 'X-Auth-Token': process.env.FOOTBALL_API_KEY } }
+    );
+    const data = await response.json();
+    res.status(200).json(data);
+  } catch(e) {
+    res.status(500).json({ error: e.message });
+  }
 }
